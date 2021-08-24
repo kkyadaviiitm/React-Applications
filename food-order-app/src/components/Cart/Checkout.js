8d7c650bb8cb08ea4233595854d37232b4618c3a
Krishna <kkyadaviiitm@gmail.com>
2021-08-24 14:33:17 +0530

    intial message

---
+++ food-order-app/src/components/Cart/Checkout.js
@@ -0,0 +1,126 @@
+import { useRef, useState } from 'react';
+
+import classes from './Checkout.module.css';
+
+const isEmpty = value => value.trim() === '';
+const isFiveChars = value => value.trim().length ===5;
+
+const Checkout = (props) => {
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true
+  });
+
+
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+  const enteredStreet = streetInputRef.current.value;
+  const enteredPostalCode = postalCodeInputRef.current.value;
+  const enteredCity = cityInputRef.current.value;
+
+  const enteredNameIsValid = !isEmpty(enteredName);
+  const enteredStreetIsValid = !isEmpty(enteredStreet);
+  const enteredCityIsValid = !isEmpty(enteredCity);
+  const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+ 
+   
+  setFormInputsValidity({ 
+    name: enteredNameIsValid,
+    street: enteredStreetIsValid,
+    city: enteredCityIsValid,
+    postalCode: enteredPostalCodeIsValid
+  });
+  
+  
+  
+  const formIsValid = 
+    enteredNameIsValid && 
+    enteredStreetIsValid &&
+    enteredCityIsValid &&
+    enteredPostalCodeIsValid;  
+
+    if (!formIsValid) {
+      return;
+    }
+
+    // submit cart data
+
+    props.onConfirm({
+      name: enteredName,
+      street: enteredStreetIsValid,
+      city: enteredCity,
+      postalCode: enteredPostalCode
+});
+
+
+  };
+
+  // for showing red box when input is invalid
+
+  // for name input
+ const nameControlClasses = `${classes.control} ${
+   formInputsValidity.name ? '' : classes.invalid
+ }`;
+
+ // for street
+ const streetControlClasses = `${classes.control} ${
+  formInputsValidity.street ? '' : classes.invalid
+}`;
+
+//for postalcode
+const postalCodeControlClasses = `${classes.control} ${
+  formInputsValidity.postalCode ? '' : classes.invalid
+}`;
+
+const cityControlClasses = `${classes.control} ${
+  formInputsValidity.city ? '' : classes.invalid
+}`;
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+     
+      <div className={nameControlClasses }>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' />
+        {!formInputsValidity.name && <p>Please enter a valid name!</p>}
+      </div>
+
+      <div className={streetControlClasses}>
+        <label htmlFor='street'>Street</label>
+        <input type='text' id='street' />
+        {!formInputsValidity.street && <p>Please enter a valid street!</p>}
+      </div>
+
+      <div className={postalCodeControlClasses}>
+        <label htmlFor='postal'>Postal Code</label>
+        <input type='text' id='postal' />
+        {!formInputsValidity.postalCode && <p>Please enter a valid postal code(5 characters long)!</p>}
+      </div>
+
+      <div className={cityControlClasses}>
+        <label htmlFor='city'>City</label>
+        <input type='text' id='city' />
+        {!formInputsValidity.city && <p>Please enter a valid city!</p>}
+      </div>
+
+      <div className={classes.actions}>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+
+    </form>
+  );
+};
+
+export default Checkout;
--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MealItem from '../Meals/MealItem';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
@@ -34,7 +34,71 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li><MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} /></li>);
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://food-order-app-8bc9c-default-rtdb.firebaseio.com/meals.json')
+      
+      if (!response.ok) {
+        throw new Error ( 'something went wrong!');
+      }
+      
+      
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+
+    
+      fetchMeals().catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
+  }, [])
+
+
+  if ( isLoading ) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}> 
+        <p>{httpError}</p>
+      </section>
+    )
+  }
+
+  const mealsList = meals.map((meal) => 
+    <li>
+      <MealItem
+        key={meal.id}
+        id={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    </li>);
   return (
     <section className={classes.meals}>
       <Card>
